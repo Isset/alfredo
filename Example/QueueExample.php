806c7c3f52d12b36a3543ae306600e8c1101755f
Felix Balfoort <fhjbalfoort@gmail.com>
2012-11-27 13:57:43 +0100

    added stream example

--- Example/QueueExample.php
+++ Example/QueueExample.php
@@ -5,7 +5,7 @@ include_once __DIR__ . '/../src/Alfredo/Autoloader.php';
 use Alfredo\Server;
 use Alfredo\Payload\Pdf\Convert;
 
-$server  = new Server('http://pdfapi.lokaal/app_dev.php', 'flapfloep', 'flapfloepfliep');
+$server  = new Server('http://converter.isset.nl', 'c29e794de76c0690ba09c610bfb05ea1f85c343c', 'bbec6637426a9ec44d4b609aac44a1ff42809609');
 $payload = new Convert();
 $payload->addHtml('asdfasdf')
         ->addUrl('http://www.google.nl')
--- Example/StreamExample.php
+++ Example/StreamExample.php
@@ -5,10 +5,10 @@ include_once __DIR__ . '/../src/Alfredo/Autoloader.php';
 use Alfredo\Server;
 use Alfredo\Payload\Pdf\Convert;
 
-$server  = new Server('http://pdfapi.lokaal/app_dev.php', 'flapfloep', 'flapfloepfliep');
+$server  = new Server('http://converter.isset.nl', 'c29e794de76c0690ba09c610bfb05ea1f85c343c', 'bbec6637426a9ec44d4b609aac44a1ff42809609');
 $payload = new Convert();
 $payload
-        ->addHtml(file_get_contents('test/temp/flap.html'));
+        ->addHtml(file_get_contents('test/test.html'));
 try {
     $data = $server->stream($payload);
     header('Content-type: application/pdf');
