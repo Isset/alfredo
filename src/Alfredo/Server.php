3f11eeebee3ab53498e134455c37f35de1cd42eb
Felix Balfoort <fhjbalfoort@gmail.com>
2012-12-05 15:19:32 +0100

    added token caching

--- src/Alfredo/Server.php
+++ src/Alfredo/Server.php
@@ -9,6 +9,7 @@ use Alfredo\Payload\PayloadAbstract;
 class Server
 {
 
+    private $tokenPath;
     private $api;
 
     /**
@@ -21,7 +22,8 @@ class Server
 
     public function __construct($api, $consumerKey, $privateKey, TypeInterface $interface = null)
     {
-        $this->api = rtrim($api, '/') . '/';
+        $this->tokenPath = __DIR__ . '/Cache/token';
+        $this->api       = rtrim($api, '/') . '/';
         if (empty($interface)) {
             $this->interface = new CurlPost();
         } else {
@@ -45,15 +47,21 @@ class Server
         return $this->sendRequest($this->api . 'api/queue/get', $payload);
     }
 
-    private function sendRequest($url, PayloadAbstract $payload)
+    private function sendRequest($url, PayloadAbstract $payload, $retried = false)
     {
         $response = $this->interface->sendPayload($url, $payload, array('x-auth-token' => $this->getToken()));
         /* @var $response Alfredo\Connection\ResponseInterface */
         if ($response->getStatusCode() == 200) {
             return $response->getContent();
-        } else {
-            throw new Exception($response->getContent());
+        } else if ($response->getStatusCode() == 403) {
+            if (!$retried) {
+                $this->getToken(true);
+                return $this->sendRequest($url, $payload, true);
+            }
+            unlink($this->tokenPath);
         }
+
+        throw new Exception($response->getContent());
     }
 
     public function setToken($token)
@@ -66,10 +74,15 @@ class Server
         if ($this->token && !$reGet) {
             return $this->token;
         }
+        if (file_exists($this->tokenPath) && !$reGet) {
+            return $this->token = file_get_contents($this->tokenPath);
+        }
+
         $response = $this->interface->sendPayload($this->api . 'api/login', $this->loginPayload);
 
         if ($response->getStatusCode() == 200) {
             $token       = json_decode($response->getContent());
+            file_put_contents($this->tokenPath, $token->token);
             return $this->token = $token->token;
         } else {
             throw new Exception($response->getContent());
